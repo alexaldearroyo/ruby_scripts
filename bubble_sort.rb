34c8747c8cc151cc1ce68854eb6095bed71da2c9
alexaldearroyo <alexaldearroyo@gmail.com>
2023-09-05 13:57:17 +0900

    bubble_sort.rb updated

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,16 +4,16 @@ def bubble_sort(arr)
     # Number of elements in the array
     n_elements = arr.length
 
-    swapped = true
+    swap = true
 
     # Loop through the array until no more swaps are made
-    while swapped do
-        swapped = false
+    while swap do
+        swap = false
         # Loop through the array and swap elements if they are in the wrong order
         (n_elements - 1).times do |i|
             if arr[i] > arr[i + 1]
                 arr[i], arr[i + 1] = arr[i + 1], arr[i] # Swap elements
-                swapped = true # Set swapped to true to continue the loop if a swap was made
+                swap = true # Set swapped to true to continue the loop if a swap was made
             end
         end
     end
